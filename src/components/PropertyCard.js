e6d01c033216c7d4980eff3e605d8e6727d8a013
Todd Albert <todd@bocacode.com>
2021-03-08 20:06:15 -0500

    stylized all components

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -6,13 +6,19 @@ const cardStyle = {
   width: '18rem',
 }
 
+const imgStyle = {
+  height: '300px',
+  width: '18rem',
+  backgroundColor: '#808080'
+}
+
 const defaultImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vogelhaus_Modell_Eduard_4.JPG/2560px-Vogelhaus_Modell_Eduard_4.JPG'
 
 function PropertyCard({ property }) {
   return (
       <Link to={`/properties/${property.id}`}>
         <Card style={cardStyle}>
-          <Card.Img variant="top"
+          <Card.Img variant="top" style={imgStyle}
             src={property.photo || defaultImg} />
           <Card.Body>
             <Card.Title>{property.title}</Card.Title>
